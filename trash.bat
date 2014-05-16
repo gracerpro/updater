6b936203694fca0c2c81e23b59ca06882a0dfe5e
Gracer <gracerpro@gmail.com>
2014-05-16 19:32:52 +0400

    remove extra files

--- trash.bat
+++
@@ -1,9 +0,0 @@
-del /Q "%CD%\ide\VC100\*.sdf"
-del /Q "%CD%\bin\*.pdb"
-del /Q "%CD%\bin\*.ilk"
-
-rmdir /S /Q ".\ide\VC100\ipch"
-rmdir /S /Q ".\ide\VC100\Debug"
-rmdir /S /Q ".\ide\VC100\Release"
-
-pause
